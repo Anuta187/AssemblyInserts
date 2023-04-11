f82c996da3bbe9ea896512b8f6c4e22136caf1c4
Anuta187 <115971056+Anuta187@users.noreply.github.com>
2023-04-11 06:44:05 +0200

    Add files via upload

---
+++ Laba_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+int main()
+{
+	short int a, x;
+	x = -2;
+	_asm {
+		mov ax, x
+		//x >= 0
+		cmp ax, 0
+		jl A1
+		mov cx, x
+		mov ax, x
+		mov bx, 8
+		mul cx
+		mul bx
+		jmp A
+
+		//x < 0
+		A1 :
+		mov ax, x
+			neg ax
+			mov bl, 2
+			div bl
+			mov ah, 0
+			jmp A
+
+			A :
+		mov a, ax
+	}
+	printf("%d", a);
+}
